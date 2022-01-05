91189d83c76013ae0fd94dc1b25fdc4e2449c004
Ronald Guerra <rageragg2004@gmail.com>
2022-01-05 08:50:39 -0400

    Modificacion del menu principal de la aplicacion

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from '../material/material.module';
 import { UserRoutingModule } from './user-routing.module';
 
+import { NavbarUserComponent } from './components/navbar-user/navbar-user.component';
+import { MainComponent } from './pages/main/main.component';
 import { UserListComponent } from './pages/user/user-list/user-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ManualComponent } from './pages/manual/manual.component';
+import { UserAddComponent } from './pages/user/user-add/user-add.component';
+import { TableSampleComponent } from './components/table-sample/table-sample.component';
 
 @NgModule({
   declarations: [
-    UserListComponent
+    NavbarUserComponent,
+    MainComponent,
+    UserListComponent,
+    DashboardComponent,
+    ManualComponent,
+    UserAddComponent,
+    TableSampleComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     UserRoutingModule,
     MaterialModule
   ]
